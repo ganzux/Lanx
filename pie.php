5668967f922f39a6353437f467784209fdef5ed5
Álvaro <itoito@gmail.com>
2014-06-09 22:30:47 +0200

    Update pie.php

--- pie.php
+++ pie.php
@@ -3,7 +3,7 @@
 		<div class="12u-first">
 
 			<div id="copyright">
-				&copy; 2014 LANX ABOGADOS Y CONSULTORÍA | <a href="aviso.php" class="iframe"><?= $TXT_LEGAL_ADVICE?></a>
+				&copy; <?= numberize( "2014 LANX ABOGADOS Y CONSULTORÍA ") ?>| <a href="aviso.php" class="iframe"><?= numberize( $TXT_LEGAL_ADVICE )?></a>
 			</div>
 
 		</div>
