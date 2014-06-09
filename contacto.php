59706e55326e2ec73386d31a5a553040e0f7a370
Álvaro <itoito@gmail.com>
2014-06-09 22:28:42 +0200

    Update contacto.php

--- contacto.php
+++ contacto.php
@@ -1,4 +1,7 @@
-﻿<?php session_start();
+<?php session_start();
+
+ob_start();
+
 include 'lang.php';
 $_SESSION['ceimexpage']=3;
 $_SESSION['ceimextitle']=$TXT_CONTACT_TITLE2;
@@ -18,21 +21,21 @@ $_SESSION['ceimextitle']=$TXT_CONTACT_TITLE2;
 					<div class="8u">
 						<form action="#" method="post" name="f" id="f">
 							<fieldset>
-								<legend><?= $TXT_CONTACT_PERSON ?></legend>
+								<legend><?= numberize( $TXT_CONTACT_PERSON ) ?></legend>
 								
-								<label for="nombre" accesskey="n"><?= $TXT_CONTACT_NAME ?>: </label>
-								<input type="text" id="nombre" name="nombre" tabindex="1" value="" title="<?= $TXT_CONTACT_NAME ?>" size="30"><br />
+								<label for="nombre" accesskey="n"><?= numberize( $TXT_CONTACT_NAME ) ?>: </label>
+								<input type="text" id="nombre" name="nombre" tabindex="1" value="" title="<?= numberize( $TXT_CONTACT_NAME ) ?>" size="30"><br />
 								
-								<label for="company" class="required" accesskey="m"><?= $TXT_CONTACT_COMPANY ?>: </label>
-								<input type="text" id="company" name="company" tabindex="2" title="<?= $TXT_CONTACT_COMPANY ?>" size="30"><br />
+								<label for="company" class="required" accesskey="m"><?= numberize( $TXT_CONTACT_COMPANY ) ?>: </label>
+								<input type="text" id="company" name="company" tabindex="2" title="<?= numberize( $TXT_CONTACT_COMPANY ) ?>" size="30"><br />
 
-								<label for="telefono" accesskey="t"><?= $TXT_CONTACT_PHONE ?>: </label>
-								<input type="text" id="telefono" name="telefono" tabindex="3" title="<?= $TXT_CONTACT_PHONE ?>" size="30"><br />
+								<label for="telefono" accesskey="t"><?= numberize( $TXT_CONTACT_PHONE ) ?>: </label>
+								<input type="text" id="telefono" name="telefono" tabindex="3" title="<?= numberize( $TXT_CONTACT_PHONE ) ?>" size="30"><br />
 
-								<label for="email" class="required" accesskey="a"><?= $TXT_CONTACT_MAIL ?>: </label>
-								<input type="text" id="email" name="email" tabindex="4" title="<?= $TXT_CONTACT_MAIL ?>" size="30"><br />
+								<label for="email" class="required" accesskey="a"><?= numberize( $TXT_CONTACT_MAIL ) ?>: </label>
+								<input type="text" id="email" name="email" tabindex="4" title="<?= numberize( $TXT_CONTACT_MAIL ) ?>" size="30"><br />
 								
-								<label for="cede" class="required" accesskey="d">Ceder datos a LANX: </label>
+								<label for="cede" class="required" accesskey="d"><?= numberize("Ceder datos a LANX:") ?></label>
 								<input type="checkbox" id="cede" name="cdde" tabindex="5" title="Ceder datos a LANX"><br />
 
 							</fieldset>
@@ -40,12 +43,12 @@ $_SESSION['ceimextitle']=$TXT_CONTACT_TITLE2;
 							<br />
 
 							<fieldset>
-								<legend><?= $TXT_CONTACT_COMMEN1 ?></legend>
+								<legend><?= numberize( $TXT_CONTACT_COMMEN1 ) ?></legend>
 
-								<label for="comments" accesskey="c"><?= $TXT_CONTACT_COMMEN2 ?>: </label>
-								<textarea name="comments" rows="5" cols="44" id="comments" tabindex="6" title="<?= $TXT_CONTACT_COMMEN2 ?>" ></textarea><br />
+								<label for="comments" accesskey="c"><?= numberize( $TXT_CONTACT_COMMEN2 ) ?>: </label>
+								<textarea name="comments" rows="5" cols="44" id="comments" tabindex="6" title="<?= numberize( $TXT_CONTACT_COMMEN2 ) ?>" ></textarea><br />
 
-								<a href="#" class="button" onclick="sM();"><?= $TXT_CONTACT_SEND ?></a>
+								<a href="#" class="button" onclick="sM();"><?= numberize( $TXT_CONTACT_SEND ) ?></a>
 							</fieldset>
 						</form>
 					</div>
@@ -60,3 +63,9 @@ $_SESSION['ceimextitle']=$TXT_CONTACT_TITLE2;
 
 	</body>
 </html>
+
+<?php 
+$test = ob_get_contents();
+ob_end_clean();
+echo preg_replace('/[ \t\r\n]+/', ' ', preg_replace('/\s*$^\s*/m', "\n", $test));
+?>
