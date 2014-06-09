4e58ae14c0e7196745619971e52d127f52cde1b1
Álvaro <itoito@gmail.com>
2014-06-09 22:30:10 +0200

    Update lang.php

--- lang.php
+++ lang.php
@@ -5,15 +5,30 @@ if ( $_SESSION['ceimexlang'] == '' )
 
 include 'txt_' . $_SESSION['ceimexlang'] . '.php';
 
+function getImage( $path ) {
+
+	//return $path.$V;
+
+	$type = pathinfo($path, PATHINFO_EXTENSION);
+	$data = file_get_contents( $path );
+	$base64 = 'data:image/' . $type . ';base64,' . base64_encode($data);
+	return $base64;
+  
+}
+
 function numberize( $chain ){
 	
-	if ( false )
-		return $chain;
+	//return $chain;
 	
+	mb_internal_encoding("UTF-8");
 	$ascii = NULL;
- 
-	for ($i = 0; $i < strlen($chain); $i++)  {
-		$ascii = $ascii . "&#".ord( $chain[$i] ).";"; 
+
+	for ($i = 0; $i < mb_strlen($chain); $i++) {
+		$ordi = ord( mb_substr( $chain,$i,1 ) );
+		if( ($ordi>64 && $ordi<91) || ($ordi>96 && $ordi<123) )
+			$ascii = $ascii . "&#". ( $ordi ).";"; 
+		else
+			$ascii = $ascii .  mb_substr( $chain,$i,1 );
 	}
 
 	return( $ascii );
