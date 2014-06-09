6362ae4f7a9916c9d0d899dbfcc549dcea4f5c38
Álvaro <itoito@gmail.com>
2014-06-09 22:31:42 +0200

    Update somos4.php

--- somos4.php
+++ somos4.php
@@ -1,4 +1,7 @@
 <?php session_start();
+
+ob_start();
+
 include 'lang.php';
 $_SESSION['ceimexpage']=4;
 $_SESSION['ceimextitle']=$TXT_SOMOS_TITLEUP;
@@ -18,126 +21,130 @@ $_SESSION['ceimextitle']=$TXT_SOMOS_TITLEUP;
 					<div class="8u">
 						<section class="middle-content">
 							<div style="position:absolute;width:160px;text-align:center;">
-								<img src="images/4.png" />
-								<h1 style="padding-top:10px;">Mª Sol Muñoz Bañuls</h1>
+								<img src="<?=getImage( "images/4.png" ) ?>" />
+								<h1 style="padding-top:10px;"><?= numberize("Mª Sol Muñoz Bañuls") ?></h1>
 							</div>
 							
 							<div style="margin-left:165px;">
 							
 								<H3>
-									<?=$TXT_SOMOS_4_TITLE ?>
+									<?=numberize($TXT_SOMOS_4_TITLE) ?>
 								</H3>
 								
-								<p style="line-height: 1.3em;"><?=$TXT_SOMOS_4_TXT ?></p>
+								<p style="line-height: 1.3em;"><?=numberize($TXT_SOMOS_4_TXT) ?></p>
+								
+								<p>
+								<a name="2" target="_blank" href="https://www.linkedin.com/profile/view?id=25265708"><?= numberize("Linkedin") ?><img src="<?=getImage( "images/lnkdin.png" ) ?>" style="vertical-align: middle;padding-left: 20;" /></a></h3>
+								</p>
 
-								<p><?=$TXT_SOMOS_4_TXT1 ?></p>
-								<p><?=$TXT_SOMOS_4_TXT2 ?></p>
+								<p><?=numberize($TXT_SOMOS_4_TXT1) ?></p>
+								<p><?=numberize($TXT_SOMOS_4_TXT2) ?></p>
 
-								<p><b><?=$TXT_SOMOS_4_TXTP ?></b></p>
+								<p><b><?=numberize($TXT_SOMOS_4_TXTP) ?></b></p>
 								<div style="padding-left:10%;">
-									<p><?=$TXT_SOMOS_4_TXTP1 ?></p>
-									<p><?=$TXT_SOMOS_4_TXTP2 ?></p>
-									<p><?=$TXT_SOMOS_4_TXTP3 ?></p>
-									<p><?=$TXT_SOMOS_4_TXTP4 ?></p>
-									<p><?=$TXT_SOMOS_4_TXTP5 ?></p>
-									<p><?=$TXT_SOMOS_4_TXTP6 ?></p>
+									<p><?=numberize($TXT_SOMOS_4_TXTP1) ?></p>
+									<p><?=numberize($TXT_SOMOS_4_TXTP2) ?></p>
+									<p><?=numberize($TXT_SOMOS_4_TXTP3) ?></p>
+									<p><?=numberize($TXT_SOMOS_4_TXTP4) ?></p>
+									<p><?=numberize($TXT_SOMOS_4_TXTP5) ?></p>
+									<p><?=numberize($TXT_SOMOS_4_TXTP6) ?></p>
 								</div>
 								
 								<p style="padding-top:20px;">
-									<b><?=$TXT_SOMOS_4_TXTC ?></b>
+									<b><?=numberize($TXT_SOMOS_4_TXTC) ?></b>
 								</p>
 								<div style="padding-left:10%;">
-									<p><?=$TXT_SOMOS_4_TXTC1 ?></p>
-									<p><?=$TXT_SOMOS_4_TXTC2 ?></p>
-									<p><?=$TXT_SOMOS_4_TXTC3 ?></p>
-									<p><?=$TXT_SOMOS_4_TXTC4 ?></p>
-									<p><?=$TXT_SOMOS_4_TXTC5 ?></p>
+									<p><?=numberize($TXT_SOMOS_4_TXTC1) ?></p>
+									<p><?=numberize($TXT_SOMOS_4_TXTC2) ?></p>
+									<p><?=numberize($TXT_SOMOS_4_TXTC3) ?></p>
+									<p><?=numberize($TXT_SOMOS_4_TXTC4) ?></p>
+									<p><?=numberize($TXT_SOMOS_4_TXTC5) ?></p>
 								</div>
 								
 								<p style="padding-top:20px;">
-									<b><?=$TXT_SOMOS_4_TXTXP ?></b>
+									<b><?=numberize($TXT_SOMOS_4_TXTXP) ?></b>
 								</p>
 								
 								<p style="padding-top:20px;">
-									<b><?=$TXT_SOMOS_4_TXTXP1 ?></b>
+									<b><?=numberize($TXT_SOMOS_4_TXTXP1) ?></b>
 								</p>
 								<div style="padding-left:10%;padding-top:20px;">
-									<p><?=$TXT_SOMOS_4_TXTXP11 ?></p>
-									<p><?=$TXT_SOMOS_4_TXTXP12 ?></p>
-									<p><?=$TXT_SOMOS_4_TXTXP13 ?></p>
-									<p><?=$TXT_SOMOS_4_TXTXP14 ?></p>
-									<p><?=$TXT_SOMOS_4_TXTXP15 ?></p>
-									<p><?=$TXT_SOMOS_4_TXTXP16 ?></p>
-									<p><?=$TXT_SOMOS_4_TXTXP17 ?></p>
-									<p><?=$TXT_SOMOS_4_TXTXP18 ?></p>
+									<p><?=numberize($TXT_SOMOS_4_TXTXP11) ?></p>
+									<p><?=numberize($TXT_SOMOS_4_TXTXP12) ?></p>
+									<p><?=numberize($TXT_SOMOS_4_TXTXP13) ?></p>
+									<p><?=numberize($TXT_SOMOS_4_TXTXP14) ?></p>
+									<p><?=numberize($TXT_SOMOS_4_TXTXP15) ?></p>
+									<p><?=numberize($TXT_SOMOS_4_TXTXP16) ?></p>
+									<p><?=numberize($TXT_SOMOS_4_TXTXP17) ?></p>
+									<p><?=numberize($TXT_SOMOS_4_TXTXP18) ?></p>
 								</div>
 								
 								<p style="padding-top:20px;">
-									<b><?=$TXT_SOMOS_4_TXTXP2 ?></b>
+									<b><?=numberize($TXT_SOMOS_4_TXTXP2) ?></b>
 									<br />
-									<?=$TXT_SOMOS_4_TXTXP2a ?>
+									<?=numberize($TXT_SOMOS_4_TXTXP2a) ?>
 									
 								</p>
 								<div style="padding-left:10%;padding-top:20px;">
-									<p><?=$TXT_SOMOS_4_TXTXP3 ?></p>
-									<p><?=$TXT_SOMOS_4_TXTXP3x ?></p>
+									<p><?=numberize($TXT_SOMOS_4_TXTXP3) ?></p>
+									<p><?=numberize($TXT_SOMOS_4_TXTXP3x) ?></p>
 								</div>
 								
 								<p style="padding-top:20px;">
-									<b><?=$TXT_SOMOS_4_TXTXP2 ?></b>
+									<b><?=numberize($TXT_SOMOS_4_TXTXP2) ?></b>
 									<br />
-									<?=$TXT_SOMOS_4_TXTXP2a ?>
+									<?=numberize($TXT_SOMOS_4_TXTXP2a) ?>
 									
 								</p>
 								<div style="padding-left:10%;padding-top:20px;">
-									<p><?=$TXT_SOMOS_4_TXTXP31 ?></p>
-									<p><?=$TXT_SOMOS_4_TXTXP32 ?></p>
-									<p><?=$TXT_SOMOS_4_TXTXP33 ?></p>
-									<p><?=$TXT_SOMOS_4_TXTXP34 ?></p>
+									<p><?=numberize($TXT_SOMOS_4_TXTXP31) ?></p>
+									<p><?=numberize($TXT_SOMOS_4_TXTXP32) ?></p>
+									<p><?=numberize($TXT_SOMOS_4_TXTXP33) ?></p>
+									<p><?=numberize($TXT_SOMOS_4_TXTXP34) ?></p>
 								</div>
 								
 								<p style="padding-top:20px;">
-									<b><?=$TXT_SOMOS_4_TXTXP4 ?></b>
+									<b><?=numberize($TXT_SOMOS_4_TXTXP4) ?></b>
 									
 								</p>
 								<div style="padding-left:10%;padding-top:20px;">
-									<p><?=$TXT_SOMOS_4_TXTXP41 ?></p>
-									<p><?=$TXT_SOMOS_4_TXTXP42 ?></p>
-									<p><?=$TXT_SOMOS_4_TXTXP43 ?></p>
-									<p><?=$TXT_SOMOS_4_TXTXP44 ?></p>
+									<p><?=numberize($TXT_SOMOS_4_TXTXP41) ?></p>
+									<p><?=numberize($TXT_SOMOS_4_TXTXP42) ?></p>
+									<p><?=numberize($TXT_SOMOS_4_TXTXP43) ?></p>
+									<p><?=numberize($TXT_SOMOS_4_TXTXP44) ?></p>
 								</div>
 								
 								<p style="padding-top:20px;">
-									<b><?=$TXT_SOMOS_4_TXTXP5 ?></b>
+									<b><?=numberize($TXT_SOMOS_4_TXTXP5) ?></b>
 									
 								</p>
 								<div style="padding-left:10%;padding-top:20px;">
-									<p><?=$TXT_SOMOS_4_TXTXP51 ?></p>
-									<p><?=$TXT_SOMOS_4_TXTXP52 ?></p>
+									<p><?=numberize($TXT_SOMOS_4_TXTXP51) ?></p>
+									<p><?=numberize($TXT_SOMOS_4_TXTXP52) ?></p>
 								</div>
 								
 								<p style="padding-top:20px;">
-									<b><?=$TXT_SOMOS_4_TXTXP6 ?></b>
+									<b><?=numberize($TXT_SOMOS_4_TXTXP6) ?></b>
 								</p>
 								
 								
 								<p style="padding-top:20px;">
-									<b><?=$TXT_SOMOS_4_TXTF ?></b>
+									<b><?=numberize($TXT_SOMOS_4_TXTF) ?></b>
 								</p>
 								<div style="padding-left:10%;">
-									<p><?=$TXT_SOMOS_4_TXTF1 ?></p>
-									<p><?=$TXT_SOMOS_4_TXTF2 ?></p>
-									<p><?=$TXT_SOMOS_4_TXTF3 ?></p>
-									<p><?=$TXT_SOMOS_4_TXTF4 ?></p>
-									<p><?=$TXT_SOMOS_4_TXTF5 ?></p>
+									<p><?=numberize($TXT_SOMOS_4_TXTF1) ?></p>
+									<p><?=numberize($TXT_SOMOS_4_TXTF2) ?></p>
+									<p><?=numberize($TXT_SOMOS_4_TXTF3) ?></p>
+									<p><?=numberize($TXT_SOMOS_4_TXTF4) ?></p>
+									<p><?=numberize($TXT_SOMOS_4_TXTF5) ?></p>
 								</div>
 								
 								<p style="padding-top:20px;">
-									<b><?=$TXT_SOMOS_4_TXTCER ?></b>
+									<b><?=numberize($TXT_SOMOS_4_TXTCER) ?></b>
 								</p>
 								<div style="padding-left:10%;">
-									<p><?=$TXT_SOMOS_4_TXTCER1 ?></p>
-									<p><?=$TXT_SOMOS_4_TXTCER2 ?></p>
+									<p><?=numberize($TXT_SOMOS_4_TXTCER1) ?></p>
+									<p><?=numberize($TXT_SOMOS_4_TXTCER2) ?></p>
 								</div>
 
 							</div>
@@ -156,3 +163,9 @@ $_SESSION['ceimextitle']=$TXT_SOMOS_TITLEUP;
 	</body>
 
 </html>
+
+<?php 
+$test = ob_get_contents();
+ob_end_clean();
+echo preg_replace('/[ \t\r\n]+/', ' ', preg_replace('/\s*$^\s*/m', "\n", $test));
+?>
