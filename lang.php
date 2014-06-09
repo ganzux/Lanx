9f64a6a7b051f7b3e5cd64c2c95796493bbda377
Álvaro <itoito@gmail.com>
2014-06-09 12:10:17 +0100

    1st version of numberize
    Trying to obfuscate HTML

--- lang.php
+++ lang.php
@@ -1,6 +1,22 @@
 <?php
+
 if ( $_SESSION['ceimexlang'] == '' )
 	$_SESSION['ceimexlang'] = 'es';
 
 include 'txt_' . $_SESSION['ceimexlang'] . '.php';
+
+function numberize( $chain ){
+	
+	if ( false )
+		return $chain;
+	
+	$ascii = NULL;
+ 
+	for ($i = 0; $i < strlen($chain); $i++)  {
+		$ascii = $ascii . "&#".ord( $chain[$i] ).";"; 
+	}
+
+	return( $ascii );
+	
+}
 ?>
