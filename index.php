5d90a99332c1fd7edd066ce3a9f443b482c7b88a
Álvaro <itoito@gmail.com>
2014-06-09 22:29:53 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php session_start();
 
+ob_start();
+
 if ( isset( $_SESSION['ceimexlang'] ) )
 	$lengua = $_SESSION['ceimexlang'];
 else
@@ -32,11 +34,11 @@ $_SESSION['ceimextitle']= $TXT_INDEX_TITLE;
 					<div class="2u-first">
 
 						<section style="padding-left:12px;">
-							<h2><?=$TXT_INDEX_H0_TITLE ?></h2>
+							<h2><?= numberize( $TXT_INDEX_H0_TITLE ) ?></h2>
 							<ul class="link-list" style="padding-left:10px;">
-								<li><a href="#" id="C1"><?=$TXT_INDEX_H1_TITLE ?></a></li>
-								<li><a href="#" id="C2"><?=$TXT_INDEX_H2_TITLE ?></a></li>
-								<li><a href="#" id="C3"><?=$TXT_INDEX_H3_TITLE ?></a></li>
+								<li><a href="#" id="C1"><?= numberize( $TXT_INDEX_H1_TITLE ) ?></a></li>
+								<li><a href="#" id="C2"><?= numberize( $TXT_INDEX_H2_TITLE ) ?></a></li>
+								<li><a href="#" id="C3"><?= numberize( $TXT_INDEX_H3_TITLE ) ?></a></li>
 							</ul>
 						</section>
 					</div>
@@ -45,41 +47,49 @@ $_SESSION['ceimextitle']= $TXT_INDEX_TITLE;
 						<section class="middle-content">
 							<div id="CC0">
 								<a name="0"></a>
-								<h3><?=$TXT_INDEX_TITLE ?></h3>
-								<p><?=$TXT_INDEX_P1 ?></p>
-								<p><?=$TXT_INDEX_P2 ?></p>
+								<h3><?= numberize( $TXT_INDEX_TITLE ) ?></h3>
+								<p><?= numberize( $TXT_INDEX_P1 ) ?></p>
+								<p><?= numberize( $TXT_INDEX_P2 ) ?></p>
 								
 								<p style="padding-top: 6;padding-bottom: 6;text-align: center;font-weight: bold;border-top: 2px solid rgb(173, 173, 173);border-bottom: 2px solid rgb(173, 173, 173);">
-									<?=$TXT_INDEX_P3 ?>
+									<?= numberize( $TXT_INDEX_P3 ) ?>
 								</p>
 
 							</div>
 
 							<div id="CC1" style="display: none;">
 								<a name="1"></a>
-								<h3><?=$TXT_INDEX_H1_TITLE ?></h3>
-								<p style="padding-top: 6;padding-bottom: 6;text-align: center;font-weight: bold;border-top: 2px solid rgb(173, 173, 173);border-bottom: 2px solid rgb(173, 173, 173);"><?=$TXT_INDEX_P31 ?></p>
+								<h3><?= numberize( $TXT_INDEX_H1_TITLE ) ?></h3>
+								<p style="padding-top: 6;padding-bottom: 6;text-align: center;font-weight: bold;border-top: 2px solid rgb(173, 173, 173);border-bottom: 2px solid rgb(173, 173, 173);"><?= numberize( $TXT_INDEX_P31 ) ?></p>
 								
 								<div style="text-align:center">
-								<img src="<?=$TXT_INDEX_P30 ?>" style="width:450px;padding-bottom:20px;" />
+								<img src="<?= getImage( $TXT_INDEX_P30 ) ?>" style="width:450px;padding-bottom:20px;" />
+
 								</div>
 								
-								<p><?=$TXT_INDEX_P32 ?></p>
-								<p><?=$TXT_INDEX_P33 ?></p>
+								<p><?= numberize( $TXT_INDEX_P32 ) ?></p>
+								<p><b><?= numberize( $TXT_INDEX_P33 ) ?></b><?= numberize( $TXT_INDEX_P33b ) ?></p>
 								
 							</div>
 							
 							<div id="CC2" style="display: none;">
 								<a name="2"></a>
-								<h3><?=$TXT_INDEX_H2_TITLE ?></h3>
-								<p><?=$TXT_INDEX_P8 ?></p>
-								<p><?=$TXT_INDEX_P9 ?></p>
+								<h3><?= numberize( $TXT_INDEX_H2_TITLE ) ?></h3>
+								<p><?= numberize( $TXT_INDEX_P8 ) ?></p>
+								<p>
+									<?= numberize( $TXT_INDEX_P9 ) ?>
+									<b><?= numberize( $TXT_INDEX_P9a ) ?></b>
+									<?= numberize( $TXT_INDEX_P9b ) ?>
+									<b><?= numberize( $TXT_INDEX_P9c ) ?></b>
+									<?= numberize( $TXT_INDEX_P9d ) ?>
+								
+								</p>
 							</div>
 							
 							<div id="CC3" style="display: none;">
 								<a name="3"></a>
-								<h3><?=$TXT_INDEX_H3_TITLE ?></h3>
-								<p style="text-align:center"><img src="images/<?=$TXT_INDEX_P10 ?>" width="500" /></p>
+								<h3><?= numberize( $TXT_INDEX_H3_TITLE ) ?></h3>
+								<p style="text-align:center"><img src="<?=getImage( "images/".$TXT_INDEX_P10 ) ?>" width="500" /></p>
 							</div>
 
 						</section>
@@ -147,3 +157,9 @@ $_SESSION['ceimextitle']= $TXT_INDEX_TITLE;
 	
 	</script>
 </html>
+
+<?php 
+$test = ob_get_contents();
+ob_end_clean();
+echo preg_replace('/[ \t\r\n]+/', ' ', preg_replace('/\s*$^\s*/m', "\n", $test));
+?>
